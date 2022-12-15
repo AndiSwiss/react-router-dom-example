c7a85a5842c8382a21bf48bfd8326e41bbe06467
Andreas Ambühl <36681000+andiswiss@users.noreply.github.com>
2022-12-15 14:56:15 +0100

    refactor to resemble more our app structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import {Outlet, Link, BrowserRouter, Routes, Route} from 'react-router-dom'
+import Expenses from './routes/expenses'
+import Invoices from './routes/invoices'
+import Invoice from './routes/invoice'
+import {useState} from 'react'
 
-export default function App() {
+function Nav() {
   return (
     <div>
       <h1>Bookkeeper</h1>
@@ -9,7 +13,43 @@ export default function App() {
         <Link to="/invoices">Invoices</Link> |{' '}
         <Link to="/expenses">Expenses</Link>
       </nav>
-      <Outlet />
+      <Outlet/>
     </div>
+  )
+}
+export default function App() {
+  // Test, whether this App keeps the state => works
+  const [data, setData] = useState(1)
+
+  return (
+    <BrowserRouter>
+      <div>{data}</div>
+      <button onClick={() => setData(data + 1)}>incr</button>
+      <hr/>
+      <Routes>
+        <Route path="/" element={<Nav />}>
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="invoices" element={<Invoices />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: '1rem' }}>
+                  <p>Select an invoice</p>
+                </main>
+              }
+            />
+            <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
